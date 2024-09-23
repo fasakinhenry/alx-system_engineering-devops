9adadca18fb1fdcb7ae49c878c8ab44110bcf089
Fasakin Henry <fasakinhenry@gmail.com>
2024-09-23 14:29:46 +0100

    feat: update task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -11,7 +11,7 @@ def number_of_subscribers(subreddit):
     """Returns the total number of subscribers on a given subreddit"""
     URL = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/HairGlittering6558)"
     }
     response = requests.get(URL, headers=headers, allow_redirects=False)
     if response.status_code == 404:
